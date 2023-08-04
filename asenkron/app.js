fc9bddb16867cf6acd278c500bee4c6f0b53b1c2
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-04 16:47:28 +0300

    hava_durumu_uygulamasi_1

--- asenkron/app.js
+++ asenkron/app.js
@@ -2,11 +2,27 @@
 // var url = "https://api.openweathermap.org/data/2.5/weather?q=istanbul,tr&appid=5c1093a6f30891eda04dc3db21ea5e48&units=metric"
 
 var weather = require("./weather");
+var location = require("./location");
 
 weather(function(currentWeather){
     console.log(currentWeather);
 });
 
+location(function(location){
+
+    if(!location){
+
+        console.log("Lokasyon bilgisi alinamadi!!!")
+        return;
+
+    } else {
+
+        console.log("Sehir : " + location.city);
+        console.log("Long/Lat : " + location.loc);
+        console.log("Ulke : " + location.country);
+        
+    }
+})
 
 /*
 request({
