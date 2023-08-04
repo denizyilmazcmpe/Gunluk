a251b097eb1f96af5f455dc8b63d240c79915bb1
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-04 15:15:17 +0300

    call_back_fonksiyonlar_ve_hava_durumu_uygulaması

---
+++ asenkron/example-callbacks.js
@@ -0,0 +1,18 @@
+var request = require("request");
+var url = "https://api.openweathermap.org/data/2.5/weather?q=istanbul,tr&appid=5c1093a6f30891eda04dc3db21ea5e48&units=metric"
+
+request({
+    url : url,
+    json : true,
+}, function(error, response, body){
+
+    if(error){
+        
+        console.log("Hava durumu alinamadi!!")
+    
+    } else {
+
+        console.log(body.name + "'da hava sicakligi : " + body.main.temp);
+
+    }
+});
