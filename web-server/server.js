da1b7eeb2154b43c8ad9977ab4b250aba4908674
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-07 12:58:13 +0300

    statik_web_sayfalarini_yayinlama

--- web-server/server.js
+++ web-server/server.js
@@ -1,12 +1,22 @@
 var express = require("express");
 var app = express();
+var PORT = 3000;
 
+//localhost:3000
+/*
 app.get("/", function(req, res){
     res.send(" Merhaba Express!!!!")
 })
+*/
 
 app.get("/hakkimda", function(req, res){
     res.send("Hakkımda sayfası!!!");
 })
 
-app.listen(3000);
+app.use(express.static(__dirname + '/public'));
+
+// console.log(__dirname);
+
+app.listen(/*3000*/PORT, function(){
+    console.log('Express server' + PORT + 'nolu portta çalışıyor....');
+});
