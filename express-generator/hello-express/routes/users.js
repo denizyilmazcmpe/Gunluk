249d1ecf4554b9c0d0f9c7c99cd8243817f45740
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-07 17:10:59 +0300

    express_responde_nesnesi

--- express-generator/hello-express/routes/users.js
+++ express-generator/hello-express/routes/users.js
@@ -4,7 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   var id = req.query.id;
-  res.send('Göndermiş olduğunuz parametre: ' + id);
+  // res.send('Göndermiş olduğunuz parametre: ' + id);
+  // res.render("index", {title : "Kullanıcı Listesi"});
+  res.redirect("http://google.com.tr");
 });
 
 module.exports = router;
