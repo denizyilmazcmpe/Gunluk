51c895984ce60705fccd0ae5d017e99bf0199089
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-08 17:37:32 +0300

    remove_komutu

--- mongoose/app.js
+++ mongoose/app.js
@@ -1,27 +1,42 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 mongoose.connect('mongodb://127.0.0.1:27017/nodeMongoTest', { useNewUrlParser: true, useUnifiedTopology: true });
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 
-var User = require("./user");
+const User = require("./user");
 
 db.on('error', console.error.bind(console, 'Bağlantı hatası:'));
-db.once('open', function() {
-    console.log("Veritabanına bağlantı sağlandı!!");
-/*   
-    var user_1 = new User({
-        ad : "Deniz",
-        soyad : "Yılmaz",
-        dogumTarihi : "10/12/1995",
-        email : "denizyilmazcmpe@gmail.com"
-    });
+db.once('open', async function () {
+    try {
+        console.log("Veritabanına bağlantı sağlandı!!");
 
-    user_1.save().then( res => {
-        console.log('kullanıcı kaydedildi!!');
-    });
+/*
+        // Kullanıcı ekleme işlemi
+        const user_1 = new User({
+            ad: "Deniz",
+            soyad: "Yılmaz",
+            dogumTarihi: "10/12/1995",
+            email: "denizyilmazcmpe@gmail.com"
+        });
+
+        await user_1.save();
+        console.log('Kullanıcı kaydedildi!!');
 */
 
-    User.find({}).then( res2 => {
-        console.log(res2);
-    })
+        // Tüm kullanıcıları listeleme işlemi
+        const allUsers = await User.find({});
+        console.log(allUsers);
+
+        // Kullanıcı silme işlemi
+        const deletedUser = await User.findOneAndDelete({ _id: "64d2461426ade4d5bff4f84a" });
+        if (deletedUser) {
+            console.log('Kayıt silindi!!');
+            const remainingUsers = await User.find({});
+            console.log(remainingUsers);
+        } else {
+            console.log('Kayıt bulunamadı veya silinemedi.');
+        }
+    } catch (error) {
+        console.error('İşlem hatası:', error);
+    }
 });
