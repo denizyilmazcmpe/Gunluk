26ccc07b009266e43a3296deef8252689894940e
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-08 15:26:43 +0300

    mongodb_find_komutu

--- mongodb/find.js
+++ mongodb/find.js
@@ -2,12 +2,13 @@ var MongoDB = require("mongodb").MongoClient;
 
 MongoDB.connect('mongodb://127.0.0.1:27017/nodeMongoTest').then(res => {
     console.log("Bağlantı Sağlandı!!!");
-    const db = res.db("nodeMongoTest");
-    db.collection("kitap").findOne({}).then(row => {
 
-        console.log(row);
+    const db = res.db("nodeMongoTest");
+    db.collection("kitap").find({basim_yili : 2017}).toArray().then(res2 => {
+
+        console.log(res2);
     })
-  
+
 }).catch(error => {
     console.error("Bağlantı Sağlanamadı!!!", error);
 });
