52063e8e3919eae467c4fcbf8d58e53ee89cea4f
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-09 14:01:11 +0300

    select_islemi

---
+++ nodejs-mysql/select.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "root",
+  database : "nodejs"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Bağlantı başarılı..");
+
+  connection.query("SELECT * from personel", function(err, result){
+
+    if(err) throw err;
+
+    // console.log("Result : " + result);
+
+    result.forEach(element => {
+
+        console.log(element.ad + "-" + element.soyad);
+        
+    });
+  })
+});
