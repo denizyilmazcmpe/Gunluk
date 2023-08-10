9f9a9ba92c111283e5050deec597ac4674ef049b
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-10 09:44:31 +0300

    create_komutu

--- sequelize/server.js
+++ sequelize/server.js
@@ -1,41 +1,50 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('todo_api', 'root', 'root',{
-    host : 'localhost',
-    dialect : 'mysql',
+const sequelize = new Sequelize('todo_api', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql',
 });
 
 const User = sequelize.define('user', {
-    username : {
-        type : Sequelize.STRING,
-        allowNull : false,
-        validate : {
-            len : [1,10]
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1, 10]
         },
         //defaultValue : "Safideha"
     },
-    email : {
-        type : Sequelize.STRING,
-        allowNull : false,
-        unique : true,
-        validate : {
-            isEmail : true
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
         }
     },
-    phone : {
-        type : Sequelize.STRING,
-        allowNull : true,
-        validate : {
-            len : [1,20]
+    phone: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        validate: {
+            len: [1, 20]
         }
     },
-    isActive : {
-        type : Sequelize.BOOLEAN,
-        defaultValue : true
+    isActive: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
     }
 });
 
-sequelize.sync({
-    force : true
-}).then(() => {
+sequelize.sync({ force: false }).then(() => {
     console.log('Veritabanına baglanti saglandi...');
+
+    User.create({
+        username : "Sahil",
+        email : "sahilyilmaz@gmail.com",
+        phone : "5325425263",
+        isActive : false
+    }).then(
+        row => {
+            console.log(row.toJSON());
+        })
+
 })
