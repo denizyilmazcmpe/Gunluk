18b86f244f16ffe92b0c322ede75a6cfc859004d
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-10 09:31:40 +0300

    sequelize_ile_field_islemleri

--- sequelize/server.js
+++ sequelize/server.js
@@ -5,10 +5,37 @@ const sequelize = new Sequelize('todo_api', 'root', 'root',{
 });
 
 const User = sequelize.define('user', {
-    username : Sequelize.STRING,
-    email : Sequelize.STRING
+    username : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        validate : {
+            len : [1,10]
+        },
+        //defaultValue : "Safideha"
+    },
+    email : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : true,
+        validate : {
+            isEmail : true
+        }
+    },
+    phone : {
+        type : Sequelize.STRING,
+        allowNull : true,
+        validate : {
+            len : [1,20]
+        }
+    },
+    isActive : {
+        type : Sequelize.BOOLEAN,
+        defaultValue : true
+    }
 });
 
-sequelize.sync().then(() => {
+sequelize.sync({
+    force : true
+}).then(() => {
     console.log('Veritabanına baglanti saglandi...');
 })
