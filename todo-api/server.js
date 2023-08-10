6804a5bf48070260e21032f0aef65b33b7753af6
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-10 13:09:27 +0300

    request_metodlarının_tanimlanmasi

--- todo-api/server.js
+++ todo-api/server.js
@@ -6,12 +6,29 @@ var PORT = 3000;
 var db = require("./db");
 /******************************************************/
 
+// GET /todos : Listeleme
 app.get("/todos", function(req, res){
 
-    res.send("tüm todo elemanları listelenir...");
-
+    res.send("get metoduyla listeleme işlemi yapılır...");
 })
 
+// POST /todos : Kaydetme
+app.post("/todos", function(req, res){
+
+    res.send("post metoduyla kayıt işlemi yapılır...")
+})
+
+// PUT /todos : Güncelleme
+app.put("/todos/:id", function(req, res){
+
+    res.send("put metoduyla update işlemi yapılır...")
+})
+
+// DELETE /todos : Silme
+app.delete("/todos/:id", function(req, res){
+
+    res.send("delete metoduyla silme işlemi yapılır...")
+})
 
 db.sequelize.sync().then(function(){
     console.log('Database bağlantısı başarılıdır');
